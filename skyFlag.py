2dda86b9b69acce7af47d30736c5fe65d8274897
Faruk Karaman <134199695+FarukKaramanVektorel@users.noreply.github.com>
2023-05-22 20:09:19 +0300

    Sky Flag in Turtle Library

--- skyFlag.py
+++ skyFlag.py
@@ -4,21 +4,17 @@ s = Screen()
 s.title("Sky Flag")
 s.setup(width=720, height=420)
 s.bgcolor("#5299db")
-
 t.up()
 t.goto(-100, -100)
 t.color('white')
 t.begin_fill()
 t.circle(120)
 t.end_fill()
-
 t.goto(-70, -60)
 t.color('#5299db')
 t.begin_fill()
 t.circle(100)
 t.end_fill()
-
-
 t.goto(-30, 125)
 t.fillcolor("white")
 t.begin_fill()
@@ -26,7 +22,6 @@ for i in range(5):
     t.forward(150)
     t.right(144)
 t.end_fill()
-
 t.goto(-320, -190)
 t.color("white")
 t.write("",
